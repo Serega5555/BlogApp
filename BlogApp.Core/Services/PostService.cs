ad018c7957b595a88096116dba6ac7077cd4c23e
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:38:04 +0300

    Create PostService.cs

---
+++ BlogApp.Core/Services/PostService.cs
@@ -0,0 +1,92 @@
+﻿using BlogApp.InterfaceServices;
+using BlogApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Data;
+
+namespace BlogApp.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly BlogContext _context;
+
+        public PostService(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Post> CreateAsync(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+
+        public async Task<Post> GetByIdWithDetailsAsync(Guid id)
+        {
+            return await _context.Posts
+        .Include(p => p.Author)
+        .Include(p => p.Comments)
+            .ThenInclude(c => c.Author) // <-- Это нужно
+        .Include(p => p.PostTags)
+            .ThenInclude(pt => pt.Tag)
+        .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Post>> GetAllAsync()
+        {
+            return await _context.Posts
+            .Include(p => p.Author)
+            .Include(p => p.Comments)
+            .Include(p => p.PostTags)
+                .ThenInclude(pt => pt.Tag)
+            .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Post>> GetAllWithTagsAsync()
+        {
+            return await _context.Posts
+                .Include(p => p.PostTags)
+                .ThenInclude(pt => pt.Tag)
+                .OrderByDescending(p => p.CreatedDate)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Post>> GetByAuthorAsync(string authorId)
+        {
+            return await _context.Posts
+            .Where(p => p.AuthorId == authorId)
+            .Include(p => p.Author)
+            .Include(p => p.Comments)
+            .Include(p => p.PostTags)
+                .ThenInclude(pt => pt.Tag)
+            .ToListAsync();
+        }
+
+        public async Task<Post> GetByIdAsync(Guid id)
+        {
+            return await _context.Posts
+            .Include(p => p.Author)
+            .Include(p => p.Comments)
+            .Include(p => p.PostTags)
+                .ThenInclude(pt => pt.Tag)
+            .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task UpdateAsync(Post post)
+        {
+            _context.Posts.Update(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync (Guid id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
