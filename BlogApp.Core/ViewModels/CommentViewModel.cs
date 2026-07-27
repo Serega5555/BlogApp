65f7fd6c7f81b978b5bb373df5d631479b3e03f3
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:41:47 +0300

    Create CommentViewModel.cs

---
+++ BlogApp.Core/ViewModels/CommentViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogApp.ViewModels
+{
+    public class CommentViewModel
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }  
+        public DateTime CreatedDate { get; set; }
+        public Guid PostId { get; set; }
+        public string PostTitle { get; set; } // Добавляем заголовок статьи для удобства отображения
+        public bool CanDelete { get; set; } // Свойство для проверки возможности удаления комментария
+        public bool CanEdit { get; set; }
+        public string AuthorId { get; set; } // Добавляем для проверки авторства
+    }
+}
