82d6f27fb702e7e22aa67ef76560adec88d472b7
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:45:24 +0300

    Create PostListViewModel.cs

---
+++ BlogApp.Core/ViewModels/PostListViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogApp.ViewModels
+{
+    public class PostListViewModel
+    {
+        public IEnumerable<PostItemViewModel> Posts { get; set; }
+
+        public class PostItemViewModel
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public int ViewCount { get; set; }
+            public IEnumerable<string> Tags { get; set; }
+        }
+    }
+}
