899144176c719a99cf875ba8f12749d6315a79a5
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:34:43 +0300

    Create ICommentService.cs

---
+++ BlogApp.Core/InterfaceServices/ICommentService.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Data.Models;
+using BlogApp.ViewModels;
+using System.Security.Claims;
+
+namespace BlogApp.InterfaceServices
+{
+    public interface ICommentService
+    {
+        Task<Comment> CreateAsync(Comment comment);
+        Task<IEnumerable<Comment>> GetAllAsync();
+        Task<Comment> CreateFromViewAsync(Guid postId, string content, string authorId);
+        Task<Comment> GetByIdAsync(Guid id);
+        Task UpdateAsync (Comment comment);
+        Task<IEnumerable<CommentViewModel>> GetAllCommentsWithViewModelAsync(ClaimsPrincipal user);
+        Task DeleteAsync (Guid id);
+    }
+}
