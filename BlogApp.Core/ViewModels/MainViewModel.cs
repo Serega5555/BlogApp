c851c42b12bbcb14eaa3675ad77de575f88d36f4
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:45:04 +0300

    Create MainViewModel.cs

---
+++ BlogApp.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public LoginViewModel LoginViewModel { get; set; }
+        public RegisterViewModel RegisterViewModel { get; set; }
+        public string ActiveForm { get; set; } = "login";
+
+        public MainViewModel()
+        {
+            LoginViewModel = new LoginViewModel();
+            RegisterViewModel = new RegisterViewModel();
+        }
+    }
+}
