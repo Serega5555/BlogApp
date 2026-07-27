c35625dfb3ced436bf533f69fa73845271732049
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:44:43 +0300

    Create LoginViewModel.cs

---
+++ BlogApp.Core/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Введите свое имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
