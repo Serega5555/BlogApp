728a70c7bd7d4ca61389c09cde03b0da6ff1353a
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:47:26 +0300

    Create UserListViewModel.cs

---
+++ BlogApp.Core/ViewModels/UserListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Data.Models;
+
+namespace BlogApp.ViewModels
+{
+    public class UserListViewModel
+    {
+        public IEnumerable<UserItemViewModel> Users { get; set; }
+        public List<Role> AvailableRoles { get; set; }
+    }
+
+    public class UserItemViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public Dictionary<string, string> RoleDescriptions { get; set; } // Описания ролей
+    }
+}
