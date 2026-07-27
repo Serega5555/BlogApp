63469a2e14047f6237eb31eb718883ba9c22598c
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:36:42 +0300

    Create IUserProfileService.cs

---
+++ BlogApp.Core/InterfaceServices/IUserProfileService.cs
@@ -0,0 +1,11 @@
+﻿using BlogApp.Data.Models;
+using BlogApp.ViewModels;
+
+namespace BlogApp.InterfaceServices
+{
+    public interface IUserProfileService
+    {
+        Task<UserProfileViewModel> GetUserProfileAsync(string userId);
+        Task<ApplicationUser> GetUserWithPostsAndTagsAsync(string userId);
+    }
+}
