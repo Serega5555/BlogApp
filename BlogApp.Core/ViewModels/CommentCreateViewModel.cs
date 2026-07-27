57dadc43b6c6b3bda7deae4a9f4d564e9cc89ddc
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:40:54 +0300

    Create CommentCreateViewModel.cs

---
+++ BlogApp.Core/ViewModels/CommentCreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class CommentCreateViewModel
+    {
+        [Required]
+        [StringLength(200, ErrorMessage = "Название тега должно быть до 200 символов")]
+        public string Content { get; set; }
+        [Required]
+        public Guid PostId { get; set; }
+    }
+}
