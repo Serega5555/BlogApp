a428df20482a85ae11ea281060c927dbde9ac557
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:40:25 +0300

    Create ChangePasswordViewModel.cs

---
+++ BlogApp.Core/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Текущий пароль обязателен")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "Новый пароль обязателен")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Пароль должен быть от 6 до 100 символов")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Подтверждение пароля обязательно")]
+        [Compare("NewPassword", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
