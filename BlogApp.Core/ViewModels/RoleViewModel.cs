1c27d3df918d263fb7ebcede479412aeeb10d47c
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:46:19 +0300

    Create RoleViewModel.cs

---
+++ BlogApp.Core/ViewModels/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Введите название роли")]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
