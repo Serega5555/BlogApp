792c3000d695f0aea37c7f4cac8a26f95b911890
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:35:32 +0300

    Create IPostTagService.cs

---
+++ BlogApp.Core/InterfaceServices/IPostTagService.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Data.Models;
+
+namespace BlogApp.InterfaceServices
+{
+    public interface IPostTagService
+    {
+        Task AddTagToPostAsync(Guid postId, Guid tagId);
+        Task<IEnumerable<Tag>> GetTagsForPostAsync(Guid postId);
+        Task<int> GetPostCountForTagAsync(Guid tagId);
+        Task RemoveTagFromPostAsync(Guid postId, Guid tagId);
+    }
+}
