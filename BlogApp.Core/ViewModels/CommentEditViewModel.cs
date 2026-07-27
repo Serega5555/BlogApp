2bd9c81ab3944ba3fccb4c9a5b2566fea099498f
Serega5555 <lazarpalace@gmail.com>
2026-07-27 16:41:18 +0300

    Create CommentEditViewModel.cs

---
+++ BlogApp.Core/ViewModels/CommentEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class CommentEditViewModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Комментарий не может быть пустым")]
+        [StringLength(100, ErrorMessage = "Комментарий слишком длинный")]
+        public string Content { get; set; }
+        public Guid PostId { get; set; }
+    }
+}
