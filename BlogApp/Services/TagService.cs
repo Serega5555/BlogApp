eb6af4496f0d0c3265db87887003f2e507c0ade2
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:01:49 +0300

    Create TagService.cs

---
+++ BlogApp/Services/TagService.cs
@@ -0,0 +1,56 @@
+﻿using BlogApp.InterfaceServices;
+using BlogApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Data;
+
+namespace BlogApp.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly BlogContext _context;
+
+        public TagService(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Tag> CreateAsync(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+            return tag;
+        }
+
+        public async Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            return await _context.Tags
+                .Include(t => t.PostTags)
+                .ThenInclude(pt => pt.Post)
+                .ToListAsync();
+        }
+
+        public async Task<Tag> GetByIdAsync(Guid id)
+        {
+            return await _context.Tags
+                .Include(t => t.PostTags)
+                .ThenInclude(pt => pt.Post)
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task UpdateAsync (Tag tag)
+        {
+            _context.Tags.Update(tag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag != null)
+            {
+                _context.Tags.Remove(tag);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
