71aa8cef97053ddc9cf6762662a16d83ce4a4e2e
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:20:46 +0300

    Create ApplicationUser.cs

---
+++ BlogApp.Data/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlogApp.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FullName { get; set; }
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
