150805fe637478583979681222ee725da56bd363
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:21:34 +0300

    Create Post.cs

---
+++ BlogApp.Data/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogApp.Data.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int ViewCount { get; set; } = 0;
+
+        public string AuthorId { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<PostTag> PostTags { get; set; }
+    }
+}
