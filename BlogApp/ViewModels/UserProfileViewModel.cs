68fce9a18da768cf66af32db2707f455e0c44910
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:34:15 +0300

    Create UserProfileViewModel.cs

---
+++ BlogApp/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Data.Models;
+
+namespace BlogApp.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Comment> Comments { get; set; }
+        public int TotalPosts => Posts?.Count ?? 0;
+        public int TotalTags => Tags?.Count ?? 0;
+        public int TotalComments => Comments?.Count ?? 0;
+    }
+}
