7151d0ba7d4ecd355125f3660f64ceb4d28441c9
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:30:07 +0300

    Create CommentEditDto.cs

---
+++ BlogApp/DTOs/CommentEditDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class CommentEditDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Комментарий не может быть пустым")]
+        [StringLength(100, ErrorMessage = "Комментарий слишком длинный")]
+        public string Content { get; set; }
+
+        public Guid PostId { get; set; }
+    }
+}
