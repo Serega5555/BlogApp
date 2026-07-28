fa75c1848bd28885cf6a2cf40706151ac436fa90
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:32:17 +0300

    Create RegisterViewModel.cs

---
+++ BlogApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required (ErrorMessage = "Имя пользователя обязательно")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Имя пользователя долнжо содержать от 3 до 50 символов")]
+        public string UserName { get; set; }
+        [Required (ErrorMessage = "Email обязателен")]
+        [EmailAddress (ErrorMessage = "Некорректный формат Email")]
+        public string Email { get; set; }
+        [Required (ErrorMessage = "Пароль обязателен")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Пароль должен быть от 6 до 100 символов")]
+        public string Password { get; set; }
+        [Required (ErrorMessage = "Подтверждение пароля обязательно")]
+        [Compare("Password", ErrorMessage = "Пароли должны совпадать")]
+        public string ConfirmPassword { get; set; }
+    }
+}
