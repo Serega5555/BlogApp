abff312d6eb5176900ca1915ce7212d9977c903c
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:33:14 +0300

    Create UserUpdateDto.cs

---
+++ BlogApp/DTOs/UserUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.DTOs
+{
+    public class UserUpdateDto
+    {
+        [Required(ErrorMessage = "Id пользователя обязательно")]
+        public string Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Имя пользователя должно быть от 3 до 50 символов", MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Некорректный формат email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Необходимо указать хотя бы одну роль")]
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
