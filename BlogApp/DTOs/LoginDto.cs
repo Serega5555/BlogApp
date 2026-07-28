e77140974468dc16eaa8d53458f2882ffca919ef
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:31:35 +0300

    Create LoginDto.cs

---
+++ BlogApp/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Введите свое имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
