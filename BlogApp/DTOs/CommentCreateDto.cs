5dc3aec07dbfe580d1fc7c8bff0b91c214f61402
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:29:11 +0300

    Create CommentCreateDto.cs

---
+++ BlogApp/DTOs/CommentCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class CommentCreateDto
+    {
+        [Required]
+        [StringLength(200, ErrorMessage = "Комментарий должен быть до 200 символов")]
+        public string Content { get; set; }
+
+        [Required]
+        public Guid PostId { get; set; }
+    }
+}
