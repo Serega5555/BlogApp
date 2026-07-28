b95a9fbba812523e331e96071a2be560ceaa83ef
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:30:50 +0300

    Create CreatePostDto.cs

---
+++ BlogApp/DTOs/CreatePostDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class CreatePostDto
+    {
+        [Required(ErrorMessage = "Заголовок обязателен")]
+        [StringLength(100, ErrorMessage = "Заголовок не должен превышать 100 символов")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Содержание статьи обязательно")]
+        public string Content { get; set; }
+
+        public List<Guid> SelectedTagId { get; set; } = new List<Guid>();
+    }
+}
