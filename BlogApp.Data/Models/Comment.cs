2337d3f66b9c2117b205f4e9c570268cde7e1cef
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:21:12 +0300

    Create Comment.cs

---
+++ BlogApp.Data/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlogApp.Data.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        [JsonIgnore]
+        public string AuthorId { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+
+        [JsonIgnore]
+        public Guid PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
