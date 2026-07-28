0e9e2dc304a2f7e6deaa47b9c8167395f891e2d9
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:30:30 +0300

    Create CommentResponseDto.cs

---
+++ BlogApp/DTOs/CommentResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogApp.DTOs
+{
+    public class CommentResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string AuthorName { get; set; }
+        public Guid PostId { get; set; }
+    }
+}
