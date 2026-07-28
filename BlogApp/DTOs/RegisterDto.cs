3710d66a79470184c1f7258ac3dcd4721ae60848
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:32:37 +0300

    Create RegisterDto.cs

---
+++ BlogApp/DTOs/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Имя пользователя обязательно")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Имя пользователя должно содержать от 3 до 50 символов")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Некорректный формат Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Пароль должен быть от 6 до 100 символов")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Подтверждение пароля обязательно")]
+        [Compare("Password", ErrorMessage = "Пароли должны совпадать")]
+        public string ConfirmPassword { get; set; }
+    }
+}
