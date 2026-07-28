c0f6bd1bfc58121963dc2429fb3c63942f7d08f6
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:31:11 +0300

    Create EditPostDto.cs

---
+++ BlogApp/DTOs/EditPostDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class EditPostDto
+    {
+        [Required(ErrorMessage = "ID поста обязателен")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Заголовок обязателен")]
+        [StringLength(100, ErrorMessage = "Заголовок не должен превышать 100 символов")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Содержание обязательно")]
+        public string Content { get; set; }
+
+        public List<Guid> SelectedTagId { get; set; } = new List<Guid>();
+    }
+}
