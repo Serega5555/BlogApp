9890b9b089ec5b373d11ce3e26e7c72ec207e747
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:27:06 +0300

    Create CreatePostViewModel.cs

---
+++ BlogApp/ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class CreatePostViewModel
+    {
+        [Required(ErrorMessage = "Заголовок обязателен")]
+        [StringLength(100, ErrorMessage = "Заголовок не должен превышать 100 символов")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Содержание статьи обязательно")]
+        public string Content { get; set; }
+        public List<Guid> SelectedTagId { get; set; } = new List<Guid>();
+        public IEnumerable<Tag>? AvailableTags { get; set; }
+       
+    }
+}
