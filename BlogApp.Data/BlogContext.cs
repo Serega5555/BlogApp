8f69b9cc83ebd4e2d4c92e9c997db3ce7fe7b665
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:23:31 +0300

    Create BlogContext.cs

---
+++ BlogApp.Data/BlogContext.cs
@@ -0,0 +1,54 @@
+﻿using BlogApp.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data
+{
+    public class BlogContext : IdentityDbContext<ApplicationUser, Role, string>
+    {
+        public BlogContext(DbContextOptions<BlogContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PostTag>()
+                .HasKey(pt => new { pt.PostId, pt.TagId });
+
+            modelBuilder.Entity<PostTag>()
+                .HasOne(pt => pt.Post)
+                .WithMany(p => p.PostTags)
+                .HasForeignKey(pt => pt.PostId);
+
+            modelBuilder.Entity<PostTag>()
+                .HasOne(pt => pt.Tag)
+                .WithMany(t => t.PostTags)
+                .HasForeignKey(pt => pt.TagId);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Author)
+                .WithMany(u => u.Comments)
+                .OnDelete(DeleteBehavior.Restrict); // Блокировка удаления пользователя с комментариями
+
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Author)
+                .WithMany(u => u.Posts)
+                .OnDelete(DeleteBehavior.Restrict); // Блокировка удаления пользователя с постами
+        }
+
+    }
+}
