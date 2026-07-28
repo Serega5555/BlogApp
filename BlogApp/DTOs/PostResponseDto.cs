3dd984ec95d50d3f35d0323d67ddf1bfb3e84b1b
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:32:18 +0300

    Create PostResponseDto.cs

---
+++ BlogApp/DTOs/PostResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using API.DTOs;
+
+namespace BlogApp.DTOs
+{
+    public class PostResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int ViewCount { get; set; }
+        public List<string> Tags { get; set; } = new();
+        public List<CommentDto> Comments { get; set; } = new();
+    }
+}
