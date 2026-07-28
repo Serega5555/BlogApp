07058fe8bbaa4db3db6d42e6d7632f56bd669d85
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:29:42 +0300

    Create CommentDto.cs

---
+++ BlogApp/DTOs/CommentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.DTOs
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid PostId { get; set; }
+        public string PostTitle { get; set; }
+    }
+}
