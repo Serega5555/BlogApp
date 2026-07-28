8dc878be1049276427cbee4ce984964723cffaa5
Serega5555 <lazarpalace@gmail.com>
2026-07-28 13:58:07 +0300

    Create ITagService.cs

---
+++ BlogApp/InterfaceServices/ITagService.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.Data.Models;
+
+namespace BlogApp.InterfaceServices
+{
+    public interface ITagService
+    {
+        Task<Tag> CreateAsync(Tag tag);
+        Task<IEnumerable<Tag>> GetAllAsync();
+        Task<Tag> GetByIdAsync(Guid id);
+        Task UpdateAsync(Tag tag);
+        Task DeleteAsync(Guid id);
+    }
+}
