633e6bbd3cd66d1af38366d5bc8cb76c74395b6c
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:29:49 +0300

    Create EditPostViewModel.cs

---
+++ BlogApp/ViewModels/EditPostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class EditPostViewModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Заголовок обязателен")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Содержание обязательно")]
+        public string Content { get; set; }
+
+        public List<Guid> SelectedTagId { get; set; } = new List<Guid>();
+        public IEnumerable<Tag>? AvailableTags { get; set; } = new List<Tag>();
+    }
+}
