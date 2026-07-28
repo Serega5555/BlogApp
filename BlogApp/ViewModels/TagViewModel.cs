54c15762c4f57f23b92058717f3d587551a85f50
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:33:06 +0300

    Create TagViewModel.cs

---
+++ BlogApp/ViewModels/TagViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class TagViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required (ErrorMessage = "Название тега обязательно")]
+        [StringLength (50, ErrorMessage = "Название тега должно быть от 2 до 50 символов", MinimumLength = 2)]
+        public string Name { get; set; }
+        public int PostCount { get; set; } // Добавляем свойство для количества статей
+    }
+}
