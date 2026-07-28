62f106254f9ff2cc4c45adee211df07f93da2bdd
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:30:18 +0300

    Create EditUserViewModel.cs

---
+++ BlogApp/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Имя пользователя обязательно")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Некорректный формат Email")]
+        public string Email { get; set; }
+
+        public List<Role> AvailableRoles { get; set; } = new List<Role>();
+
+        public List<string> SelectedRoles { get; set; } = new List<string>();
+    }
+}
