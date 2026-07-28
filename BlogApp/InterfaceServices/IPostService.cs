f2de76a7117c93e05844bdf34b05b1b9b0e07ae5
Serega5555 <lazarpalace@gmail.com>
2026-07-28 13:54:21 +0300

    Create IPostService.cs

---
+++ BlogApp/InterfaceServices/IPostService.cs
@@ -0,0 +1,20 @@
+﻿using BlogApp.Data.Models;
+using BlogApp.DTOs;
+using System.Runtime.CompilerServices;
+
+namespace BlogApp.InterfaceServices
+{
+    public interface IPostService
+    {
+        Task<Post> CreateAsync(Post post);
+        Task<IEnumerable<Post>> GetAllAsync();
+        Task<IEnumerable<Post>> GetAllWithTagsAsync();
+        Task<IEnumerable<Post>> GetByAuthorAsync(string authorId);
+        Task<Post> GetByIdWithDetailsAsync(Guid id);
+        Task<PostResponseDto> GetPostDetailsDtoAsync(Guid id);
+        Task<IEnumerable<PostListItemDto>> GetPostListItemsByAuthorAsync(string authorId);
+        Task<Post> GetByIdAsync(Guid id);
+        Task UpdateAsync(Post post);
+        Task DeleteAsync(Guid id);
+    }
+}
