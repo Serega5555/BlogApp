4d719cf7c68f567dc8768404617734ab86774f4e
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:33:28 +0300

    Create UpdateUserViewModel.cs

---
+++ BlogApp/ViewModels/UpdateUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class UpdateUserViewModel
+    {
+        [Required(ErrorMessage = "Имя пользователя обязательно")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Имя пользователя должно быть от 3 до 50 символов")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
+        public string Email { get; set; }
+    }
+}
