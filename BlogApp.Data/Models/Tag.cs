cf92ecb8700495b7bee9ac9547e90eaf11783971
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:22:39 +0300

    Create Tag.cs

---
+++ BlogApp.Data/Models/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogApp.Data.Models
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        
+        public virtual ICollection<PostTag> PostTags { get; set; }
+    }
+}
