8e2138923854d7d0015152fec41e6c0071ff7f94
Serega5555 <lazarpalace@gmail.com>
2026-07-28 10:31:58 +0300

    Create PostListItemDto.cs

---
+++ BlogApp/DTOs/PostListItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogApp.DTOs
+{
+    public class PostListItemDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int ViewCount { get; set; }
+        public List<string> TagNames { get; set; } = new();
+    }
+}
