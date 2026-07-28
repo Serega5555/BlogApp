d93acd836ca09a4cea1e4625492996ced620a7a6
Serega5555 <lazarpalace@gmail.com>
2026-07-28 14:29:23 +0300

    Create CreateRoleViewModel.cs

---
+++ BlogApp/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required(ErrorMessage = "Введите название роли")]
+        [StringLength(50, ErrorMessage = "Название роли должно быть от 2 до 50 символов", MinimumLength = 2)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
